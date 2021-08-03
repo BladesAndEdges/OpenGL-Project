7124ec794dd777bc8f441fcb4d101c0761eec9b7
Daniel Banski <daniel.banski.1997@gmail.com>
2021-08-03 16:51:27 +0300

    Rendered a GLFW window

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
 
+#include <GLFW/glfw3.h>
+
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My Window", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
