d190ce016ae5b41e351a4fab171e7e792ca432be
Daniel Banski <daniel.banski.1997@gmail.com>
2021-08-15 23:31:49 +0300

    Obtain triangle via identity matrices inside the Uniform Buffer Object

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -4,6 +4,24 @@
 #include <GLFW/glfw3.h>
 
 #include "Shader.h"
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include "UniformBuffer.h"
+
+void framebufferCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+		(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+		type, severity, message);
+	__debugbreak();
+}
 
 
 int main(int argc, char* argv[])
@@ -12,16 +30,21 @@ int main(int argc, char* argv[])
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "My Window", nullptr, nullptr);
 
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferCallback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(MessageCallback, 0);
 
 	Shader basicShader("Shaders\\basicShader.vert", "Shaders\\basicShader.frag");
 
@@ -48,6 +71,40 @@ int main(int argc, char* argv[])
 	glEnableVertexAttribArray(1);
 
 
+	// MATRICES
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
+
+
+	glm::mat4 viewProjection = projection * view;
+
+	//UBO
+	UniformBuffer uniformBuffer;
+	copyMat4ToFloatArray(model, uniformBuffer.model);
+	copyMat4ToFloatArray(viewProjection, uniformBuffer.viewProjection);
+
+	GLuint uboMatricesID;
+
+	unsigned int uniformMatrixBlockIndex;
+	uniformMatrixBlockIndex = glGetUniformBlockIndex(basicShader.getProgramID(), "sceneMatrices");
+	assert(uniformMatrixBlockIndex != GL_INVALID_INDEX);
+
+
+	/*
+	Returns a list of specified buffer object names.
+	These "names" are integers. They are not guarranteed
+	to be a set of continuous integers.
+	*/
+	glGenBuffers(1, &uboMatricesID);
+
+	glBindBuffer(GL_UNIFORM_BUFFER, uboMatricesID);
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(uniformBuffer), &uniformBuffer, GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+	glBindBufferRange(GL_UNIFORM_BUFFER, uniformMatrixBlockIndex, uboMatricesID, 0, sizeof(UniformBuffer));
+
+	//USE glBufferSubData() to update the values of the UBO members later on whent the values change due to the addition of a camera.
 
 	while (!glfwWindowShouldClose(window))
 	{
