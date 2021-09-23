2ae411e5b968f45f23421033208650d356b4a9e5
Daniel Banski <daniel.banski.1997@gmail.com>
2021-09-24 02:00:38 +0300

    A pointer to a Material object which would point to a value in the hashmap.

--- OpenGLProject/Mesh.h
+++ OpenGLProject/Mesh.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include "Material.h"
+
 struct Mesh
 {
 	unsigned int firstIndex;
 	unsigned int vertexCount;
 
 	// Each individual mesh will have a pointer to the same material
+	Material* material;
 };
