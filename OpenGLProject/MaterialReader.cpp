f0bdb2473cc6983339067cca67cc1e1b06be9420
Daniel Banski <daniel.banski.1997@gmail.com>
2021-09-24 01:58:37 +0300

    A class that is used to parse .mtl files. The final result is a Hash Map of a materialName and Material key-value pair.

--- OpenGLProject/MaterialReader.cpp
+++ OpenGLProject/MaterialReader.cpp
@@ -1,1 +1,100 @@
 #include "MaterialReader.h"
+
+#include <iostream>
+#include <fstream>
+
+
+MaterialReader::MaterialReader()
+{
+}
+
+void MaterialReader::parseMaterialFile(const std::string & fileName)
+{
+	std::ifstream ifs(fileName);
+
+	if (!ifs.is_open())
+	{
+		throw std::exception("Could not read Material Data!");
+	}
+
+	Material currentMaterial;
+	std::string materialName;
+
+	std::string str;
+
+	bool firstMaterial = true;
+
+	while (ifs >> str)
+	{
+		if (str == "newmtl")
+		{
+			if (!firstMaterial)
+			{
+				m_Materials.insert({ materialName, currentMaterial });
+				currentMaterial.m_textures.clear();
+			}
+
+			ifs >> materialName;
+
+			firstMaterial = false;
+		}
+
+		if (str == "Ka")
+		{
+			glm::vec3 ambient;
+			ifs >> ambient.x >> ambient.y >> ambient.z;
+			currentMaterial.m_ambientColour = ambient;
+		}
+
+		if (str == "Kd")
+		{
+			glm::vec3 diffuse;
+			ifs >> diffuse.x >> diffuse.y >> diffuse.z;
+			currentMaterial.m_diffuseColour = diffuse;
+		}
+
+		if (str == "Ks")
+		{
+			glm::vec3 specular;
+			ifs >> specular.x >> specular.y >> specular.z;
+			currentMaterial.m_specularColour = specular;
+		}
+
+		if (str == "map_Ka")
+		{
+			std::string texturePath;
+			ifs >> texturePath;
+
+			const std::string path = R"(Meshes\sponza\)";
+			const std::string finalPath = path + texturePath;
+
+			Texture texture(finalPath); // Does the destructor destroy all textures?
+
+			currentMaterial.m_textures.push_back(texture);
+		}
+	}
+
+	m_Materials.insert({ materialName, currentMaterial }); // The last material will get canceled out due to ending the while loop
+}
+
+std::unordered_map<std::string, Material> MaterialReader::getMaterials() const
+{
+	return m_Materials;
+}
+
+//Ns 10.0000
+//Ni 1.5000
+//d 1.0000
+//Tr 0.0000
+//Tf 1.0000 1.0000 1.0000
+//illum 2
+//Ka 1 1 1
+//Kd 1 1 1
+//Ks 0.0000 0.0000 0.0000
+//Ke 0.0000 0.0000 0.0000
+//map_Ka textures\sponza_thorn_diff.png
+//map_Kd textures\sponza_thorn_diff.png
+//map_d textures\sponza_thorn_mask.png
+//map_bump textures\sponza_thorn_bump.png
+
+
--- OpenGLProject/MaterialReader.h
+++ OpenGLProject/MaterialReader.h
@@ -1,15 +1,23 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+#include "Material.h"
+
 class MaterialReader
 {
 private:
 
+	std::unordered_map<std::string, Material> m_Materials;
 
 public:
 
 	MaterialReader();
 
-	void parseMaterialFile();
-	void parseMaterial();
+	void parseMaterialFile(const std::string& fileName);
+
+	std::unordered_map<std::string, Material> getMaterials() const;
+
+	std::unordered_map<std::string, Material>& getReferenceToMaterials() const;
 };
 
