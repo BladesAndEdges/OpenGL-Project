0bb4c9af0e90ee8f8bda119c6b064dec27441083
Daniel Banski <daniel.banski.1997@gmail.com>
2021-09-28 07:13:01 +0300

    A function that copies memory from a glm::vec4 to a float[4] array

--- OpenGLProject/UniformBuffer.cpp
+++ OpenGLProject/UniformBuffer.cpp
@@ -9,3 +9,13 @@ void copyMat4ToFloatArray(const glm::mat4 & source, float destination[16])
 		destination[element] = matrixData[element];
 	}
 }
+
+void copyVec4ToFloatArray(const glm::vec4 & source, float destination[4])
+{
+	const float* vectorData = &source[0];
+
+	for (int element = 0; element < 4; element++)
+	{
+		destination[element] = vectorData[element];
+	}
+}
--- OpenGLProject/UniformBuffer.h
+++ OpenGLProject/UniformBuffer.h
@@ -7,9 +7,11 @@
 
 struct UniformBuffer
 {
+	float worldSpaceCameraPosition[4];
 	float model[16];
 	float viewProjection[16];
 };
 
 void copyMat4ToFloatArray(const glm::mat4& source, float destination[16]);
+void copyVec4ToFloatArray(const glm::vec4& source, float destination[4]);
 
