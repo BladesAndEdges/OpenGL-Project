3b4f162ffd76b61afa350178860ab78aff773f9e
Daniel Banski <daniel.banski.1997@gmail.com>
2021-10-07 13:51:50 +0300

    Comparison operator which is used in order to create a hash function with a Vertex attribute used within the function

--- OpenGLProject/Vertex.cpp
+++ OpenGLProject/Vertex.cpp
@@ -7,3 +7,56 @@ Vertex::Vertex() : m_position{ 0.0f, 0.0f, 0.0f }
 Vertex::Vertex(float x, float y, float z) : m_position{x, y, z}
 {
 }
+
+bool operator==(const Vertex & lhs, const Vertex & rhs)
+{
+	bool positionEqual = false;
+	bool texturesEqual = false;
+	bool normalsEqual = false;
+
+	for (unsigned int positionElement = 0; positionElement < 3; positionElement++)
+	{
+		// Position equality
+		if (lhs.m_position[positionElement] == rhs.m_position[positionElement])
+		{
+			positionEqual = true;
+		}
+		else
+		{
+			positionEqual = false;
+			break;
+		}
+	}
+
+	// Texture Coordinate equality
+	for (unsigned int textureElement = 0; textureElement < 2; textureElement++)
+	{
+
+		if (lhs.m_textureCoordinate[textureElement] == rhs.m_textureCoordinate[textureElement])
+		{
+			texturesEqual = true;
+		}
+		else
+		{
+			texturesEqual = false;
+			break;
+		}
+	}
+
+	// Normals equality
+	for (unsigned int normalElement = 0; normalElement < 3; normalElement++)
+	{
+		// Position equality
+		if (lhs.m_normal[normalElement] == rhs.m_normal[normalElement])
+		{
+			normalsEqual = true;
+		}
+		else
+		{
+			normalsEqual = false;
+			break;
+		}
+	}
+
+	return positionEqual && texturesEqual && normalsEqual;
+}
--- OpenGLProject/Vertex.h
+++ OpenGLProject/Vertex.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <functional>
+#include <string>
+#include <unordered_set>
 
 struct Vertex
 {
@@ -11,3 +17,19 @@ struct Vertex
 	Vertex(float x, float y, float z);
 };
 
+bool operator==(const Vertex& lhs, const Vertex& rhs);
+
+// class for hash function 
+struct KeyHasher
+{
+	std::size_t operator()(const Vertex& v) const
+	{
+		using std::size_t;
+		using std::hash;
+		using std::string;
+
+		return ((hash<float>()(v.m_position[0])
+			^ (hash<float>()(v.m_textureCoordinate[1]) << 1)) >> 1)
+			^ (hash<float>()(v.m_normal[2]) << 1);
+	}
+};
