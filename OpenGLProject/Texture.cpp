5ce7a6447fc49ab479e15c19fdd04ea3135ebf40
Daniel Banski <daniel.banski.1997@gmail.com>
2021-11-06 23:40:40 +0200

    Finished the texture class.
    The texture class is now effectively a wrapper over what OpenGL calls a Texture object.

--- OpenGLProject/Texture.cpp
+++ OpenGLProject/Texture.cpp
@@ -2,46 +2,90 @@
 
 #define TEXTURE_MAX_ANISOTROPY_EXT 0x84FE
 
-/*GL_NEAREST, GL_LINEAR, GL_NEAREST_MIPMAP_NEAREST, GL_NEAREST_MIPMAP_LINEAR, GL_LINEAR_MIPMAP_NEAREST, or GL_LINEAR_MIPMAP_LINEAR*/
-
-GLenum Texture::translateTargetToOpenGL(const TextureTarget & target) const
+GLenum Texture::translateTargetToOpenGL(TextureTarget target) const
 {
-	GLenum result = 0;
-
 	switch (target)
 	{
 	case TextureTarget::Texture2D:
-		result = GL_TEXTURE_2D;
+		return GL_TEXTURE_2D;
+	default:
+		assert(false);
+		return 0;
 	}
-
-	return result;
 }
 
-#include <iostream>
-
-Texture::Texture(const std::string & texturePath, const TextureTarget& textureTarget)
+GLenum Texture::translateWrapModeToOpenGL(TextureWrapMode wrapMode) const
 {
-	const GLenum target = translateTargetToOpenGL(textureTarget);
-	assert(target != 0);
+	switch (wrapMode)
+	{
+	case TextureWrapMode::Repeat:
+		return GL_REPEAT;
+	case TextureWrapMode::ClampEdge:
+		return GL_CLAMP_TO_EDGE;
+	default:
+		assert(false);
+		return 0;
+	}
+}
 
-	glCreateTextures(GL_TEXTURE_2D, 1, &m_textureID);
-	assert(m_textureID > 0);
+GLenum Texture::translateFilterModeToOpenGLMinFilter(TextureFilterMode filterMode) const
+{
+	switch (filterMode)
+	{
+	case TextureFilterMode::Point:
+		return GL_NEAREST_MIPMAP_NEAREST;
+	case TextureFilterMode::Bilinear:
+		return GL_LINEAR_MIPMAP_NEAREST;
+	case TextureFilterMode::Trilinear:
+		return GL_LINEAR_MIPMAP_LINEAR;
+	default:
+		assert(false);
+		return 0;
+	}
+}
 
-	stbi_set_flip_vertically_on_load(true);
+GLenum Texture::translateFilterModeToOpenGLMagFilter(TextureFilterMode filterMode) const
+{
+	switch (filterMode)
+	{
+	case TextureFilterMode::Point:
+		return GL_NEAREST;
+	case TextureFilterMode::Bilinear:
+	case TextureFilterMode::Trilinear:
+		return GL_LINEAR;
+	default:
+		assert(false);
+		return 0;
+	}
+}
+
+Texture::Texture(const std::string & source, TextureTarget target, TextureWrapMode wrapMode, 
+											TextureFilterMode filterMode)
+{
+	const GLenum glTarget = translateTargetToOpenGL(target);
+	const GLenum glWrapMode = translateWrapModeToOpenGL(wrapMode);
+	const GLenum glMinFilterMode = translateFilterModeToOpenGLMinFilter(filterMode);
+	const GLenum glMagFilterMode = translateFilterModeToOpenGLMagFilter(filterMode);
+
+	glGetError();
+	glCreateTextures(glTarget, 1, &m_name);
+	assert(m_name > 0);
+
+	stbi_set_flip_vertically_on_load(true); // ?
 
 	GLint width;
 	GLint height;
 	GLint numberOfChannels;
 
-	GLubyte* textureData = stbi_load(texturePath.c_str(), &width, &height, &numberOfChannels, 4); //Why does it only work when you specify as 4 channels?
+	GLubyte* textureData = stbi_load(source.c_str(), &width, &height, &numberOfChannels, 0);
 
 	m_width = (uint32_t)width;
 	m_height = (uint32_t)height;
 
 	if (textureData)
 	{
-		glTextureStorage2D(m_textureID, 1, GL_RGBA8, m_width, m_height);
-		glTextureSubImage2D(m_textureID, 0, 0, 0, m_width, m_height, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+		glTextureStorage2D(m_name, 1, GL_RGBA8, m_width, m_height);
+		glTextureSubImage2D(m_name, 0, 0, 0, m_width, m_height, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
 		stbi_image_free(textureData);
 	}
 	else
@@ -49,26 +93,35 @@ Texture::Texture(const std::string & texturePath, const TextureTarget& textureTa
 		throw std::exception("Could not load texture image!");
 	}
 
-	glObjectLabel(GL_TEXTURE, m_textureID, -1, texturePath.c_str());
+	glObjectLabel(GL_TEXTURE, m_name, -1, source.c_str());
 
-	glGenerateTextureMipmap(m_textureID);
+	glGenerateTextureMipmap(m_name);
 
-	glTextureParameteri(m_textureID, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTextureParameteri(m_textureID, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTextureParameteri(m_textureID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTextureParameteri(m_textureID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTextureParameterf(m_textureID, TEXTURE_MAX_ANISOTROPY_EXT, 16.0f);
+	glTextureParameteri(m_name, GL_TEXTURE_WRAP_S, glWrapMode);
+	glTextureParameteri(m_name, GL_TEXTURE_WRAP_T, glWrapMode);
+	glTextureParameteri(m_name, GL_TEXTURE_MIN_FILTER, glMinFilterMode);
+	glTextureParameteri(m_name, GL_TEXTURE_MAG_FILTER, glMagFilterMode);
+	glTextureParameterf(m_name, TEXTURE_MAX_ANISOTROPY_EXT, 16.0f);
+}
+
+Texture::~Texture()
+{
+	if (m_name != 0)
+	{
+		glDeleteTextures(1, &m_name);
+		m_name = 0;
+	}
 }
 
 Texture::Texture(const std::string & texturePath)
 {
 	static uint64_t textureDataUsage = 0;
 
-	glGenTextures(1, &m_textureID);
+	glGenTextures(1, &m_name);
 
-	assert(m_textureID > 0);
+	assert(m_name > 0);
 
-	glBindTexture(GL_TEXTURE_2D, m_textureID);
+	glBindTexture(GL_TEXTURE_2D, m_name);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -100,16 +153,25 @@ Texture::Texture(const std::string & texturePath)
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
+Texture::Texture(Texture && other) 
+	: m_name(other.m_name),
+	m_width(other.m_width),
+	m_height(other.m_height)
+{
+	other.m_name = 0;
+	other.m_width = 0;
+	other.m_height = 0;
+}
+
 void Texture::useTexture(GLuint textureUnit) const
 {
-	glBindTextureUnit(textureUnit, m_textureID);
+	glBindTextureUnit(textureUnit, m_name);
 }
 
-GLuint Texture::getTextureID() const
+GLuint Texture::getName() const
 {
-	return m_textureID;
+	return m_name;
 }
-
 
 uint32_t Texture::getWidth() const
 {
@@ -120,8 +182,3 @@ uint32_t Texture::getHeight() const
 {
 	return m_height;
 }
-
-//Texture::~Texture()
-//{
-//	glDeleteTextures(1, &m_textureID);
-//}
--- OpenGLProject/Texture.h
+++ OpenGLProject/Texture.h
@@ -7,31 +7,59 @@
 #include <string>
 #include <assert.h>
 
-
+// ------------------------------------------------------------------------------------------
 enum class TextureTarget
 {
 	Texture2D
 };
 
-//-------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+enum class TextureWrapMode
+{
+	Repeat, 
+	ClampEdge
+};
+
+// ------------------------------------------------------------------------------------------
+// Inspired by Unity's version on doing this
+enum class TextureFilterMode
+{
+	Point, 
+	Bilinear,
+	Trilinear
+};
+
+// ------------------------------------------------------------------------------------------
 class Texture
 {
 	private:
 
-		GLuint m_textureID;
+		GLuint m_name;
 		uint32_t m_width;
 		uint32_t m_height;
 
-		GLenum translateTargetToOpenGL(const TextureTarget& target) const;
+		// Convert details of the texture to a form OpenGL understands
+		GLenum translateTargetToOpenGL(TextureTarget target) const;
+		GLenum translateWrapModeToOpenGL(TextureWrapMode wrapMode) const;
+		GLenum translateFilterModeToOpenGLMinFilter(TextureFilterMode filterMode) const;
+		GLenum translateFilterModeToOpenGLMagFilter(TextureFilterMode filterMode) const;
 
 	public:
 
-		Texture(const std::string& textureSource);
-		Texture(const std::string& textureSource, const TextureTarget& textureTarget);
+		Texture(const std::string& source);
+		Texture(const std::string& source, TextureTarget target, TextureWrapMode wrapMode,
+									TextureFilterMode filterMode);
 
-		void useTexture(GLuint textureUnit) const;
+		~Texture();
 
-		GLuint getTextureID() const;
+		Texture(const Texture&) = delete;
+		Texture& operator=(const Texture&) = delete;
+
+		Texture(Texture&& other);
+
+		void useTexture(GLuint unit) const;
+
+		GLuint getName() const;
 		uint32_t getWidth() const;
 		uint32_t getHeight() const;
 
