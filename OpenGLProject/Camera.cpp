974b542c6cfc931db27b4f2a31995eea5d7638e1
Daniel Banski <daniel.banski.1997@gmail.com>
2021-12-25 17:00:23 +0000

    Camera class updated
    Removed the Camera class default copy constrcotr and disabled the copy assignment operator. Rearranged the code and added separating comments.

--- OpenGLProject/Camera.cpp
+++ OpenGLProject/Camera.cpp
@@ -1,10 +1,11 @@
 #include "Camera.h"
-
+// --------------------------------------------------------------------------------
 const glm::vec3 magnificationVec = glm::vec3(257.0f, 488.0f, -238.0f);
 const glm::vec3 minificationVec = glm::vec3(228.0f, 488.0f, 141.0f);
 const glm::vec3 cubePositonVec = glm::vec3(0.0f, 0.0f, -8.0f);
 const glm::vec3 cameraNormTest = glm::vec3(-950.0f, 170.0f, -58.0f);
 
+// --------------------------------------------------------------------------------
 Camera::Camera() : m_worldPosition(cameraNormTest)
 {
 	m_worldOrientation = glm::mat3(1.0f);
@@ -15,6 +16,30 @@ Camera::Camera() : m_worldPosition(cameraNormTest)
 	m_worldOrientation = glm::mat3(m);
 }
 
+// --------------------------------------------------------------------------------
+Camera::Camera(const glm::vec3 & cameraStartingPosition)
+{
+	m_worldPosition = cameraStartingPosition;
+
+	glm::mat4 m = glm::mat4(1.0f);
+	m = glm::rotate(m, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	m_worldOrientation = glm::mat3(m);
+}
+
+// --------------------------------------------------------------------------------
+Camera::Camera(const glm::vec3 & cameraStartingPosition, float cameraWidth, float cameraHeight, float cameraNearPlane, float cameraFarPlane)
+	: m_cameraWidth(cameraWidth), m_cameraHeight(cameraHeight), m_nearPlane(cameraNearPlane), m_farPlane(cameraFarPlane)
+{
+	m_worldPosition = cameraStartingPosition;
+
+	glm::mat4 m = glm::mat4(1.0f);
+	m = glm::rotate(m, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	m_worldOrientation = glm::mat3(m);
+}
+
+// --------------------------------------------------------------------------------
 glm::mat4 Camera::createViewMatrix() const
 {
 	glm::mat4 viewMatrix;
@@ -28,21 +53,25 @@ glm::mat4 Camera::createViewMatrix() const
 	return viewMatrix;
 }
 
+// --------------------------------------------------------------------------------
 glm::vec3 Camera::getWorldPosition() const
 {
 	return m_worldPosition;
 }
 
+// --------------------------------------------------------------------------------
 glm::mat3 Camera::getWorldOrientation() const
 {
 	return m_worldOrientation;
 }
 
+// --------------------------------------------------------------------------------
 void Camera::setCameraWorldPosition(const glm::vec3 & newWorldPosition)
 {
 	m_worldPosition = newWorldPosition;
 }
 
+// --------------------------------------------------------------------------------
 void Camera::setCameraWorldOrientation(const glm::mat3 & newWorldOrientation)
 {
 	m_worldOrientation = newWorldOrientation;
--- OpenGLProject/Camera.h
+++ OpenGLProject/Camera.h
@@ -6,18 +6,18 @@
 #include<gtc/matrix_transform.hpp>
 #pragma warning(pop)
 
-
+// --------------------------------------------------------------------------------
 class Camera
 {
-
-private:
-
-	glm::vec3 m_worldPosition;
-	glm::mat3 m_worldOrientation;
 
 public:
 
 	Camera();
+	Camera(const glm::vec3& cameraStartingPosition);
+	Camera(const glm::vec3& cameraStartingPosition, float cameraWidth, float cameraHeight, float cameraNearPlane, float cameraFarPlane);
+
+	Camera(const Camera&) = delete;
+	Camera& operator=(const Camera&) = delete;
 
 	glm::mat4 createViewMatrix() const;
 
@@ -26,6 +26,18 @@ public:
 
 	void setCameraWorldPosition(const glm::vec3& newWorldPosition);
 	void setCameraWorldOrientation(const glm::mat3& newWorldOrientation);
+
+private:
+
+	glm::vec3 m_worldPosition;
+	glm::mat3 m_worldOrientation;
+
+	// --------------------------------------------------------------------------------
+	// For Orthographic projection
+	float m_cameraWidth;
+	float m_cameraHeight;
+	float m_nearPlane;
+	float m_farPlane;
 
 };
 
