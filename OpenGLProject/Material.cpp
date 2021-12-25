a8011556a640fab3f212ea93ca99565ac0d85dfd
Daniel Banski <daniel.banski.1997@gmail.com>
2021-12-25 17:55:00 +0000

    Separator in the Material class

--- OpenGLProject/Material.cpp
+++ OpenGLProject/Material.cpp
@@ -1,5 +1,6 @@
 #include "Material.h"
 
+// --------------------------------------------------------------------------------
 Material::Material() : m_ambientTexture(nullptr), m_diffuseTexture(nullptr), m_specularTexture(nullptr), m_normalMapTexture(nullptr), 
 						m_maskTexture(nullptr)
 {
